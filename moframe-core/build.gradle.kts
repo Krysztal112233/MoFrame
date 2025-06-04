75114361dd772f7e0b9b347ba90bd74f9f37560e
Krysztal Huang <krysztal.huang@outlook.com>
2025-06-05 03:39:08 +0800

    Add `run-paper` for running paper via gradle

--- moframe-core/build.gradle.kts
+++ moframe-core/build.gradle.kts
@@ -1,8 +1,15 @@
 plugins {
     `java-library`
     id("io.papermc.paperweight.userdev") version "2.0.0+"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
 dependencies {
     paperweight.paperDevBundle("1.21.5-R0.1-SNAPSHOT")
+}
+
+tasks {
+    runServer {
+        minecraftVersion("1.21.5")
+    }
 }
