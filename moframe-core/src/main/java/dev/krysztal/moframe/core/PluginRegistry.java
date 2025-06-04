dfb70aa041a420db60f13355d74247150b1003b1
Krysztal Huang <krysztal.huang@outlook.com>
2025-06-04 17:22:36 +0800

    Add `PluginRegistry` for `SimpleRegistry` recording

---
+++ moframe-core/src/main/java/dev/krysztal/moframe/core/PluginRegistry.java
@@ -0,0 +1,16 @@
+// Copyright (C) 2025 KrysztalHuang <krysztal.huang@outlook.com>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// See the file LICENSE for the full license text.
+package dev.krysztal.moframe.core;
+
+import dev.krysztal.moframe.core.buff.BuffStatus;
+import dev.krysztal.moframe.core.foundation.registry.SimpleRegistry;
+
+public class PluginRegistry {
+    public static final SimpleRegistry<BuffStatus> BUFF_STATUS = new SimpleRegistry<>();
+}
